927de0d153442ec7f9fe85b31c1e4fca2e87f0a5
Ockert Fernandes <ockertfernandes@gmail.com>
2025-10-09 14:38:09 +0000

    Try fixing this error: `Runtime Error: Error: Failed to load chunk /_nex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,12 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;600;700&family=Source+Code+Pro:wght@400;500&display=swap" rel="stylesheet" />
-      </head>
-      <body suppressHydrationWarning>
+      <body>
         {children}
         <Toaster />
       </body>
