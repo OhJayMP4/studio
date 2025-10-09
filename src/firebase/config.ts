afef6d02deb2b8511e510e0c214fd26cd3b596e7
Ockert Fernandes <ockertfernandes@gmail.com>
2025-10-09 14:53:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1397195000-3cb07",
+  "appId": "1:659800845261:web:23ddaeafe5711c8b65f284",
+  "apiKey": "AIzaSyABDTJq_xSOUGLwVlR_deeIpi5rFZ4ZPUE",
+  "authDomain": "studio-1397195000-3cb07.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "659800845261"
+};
