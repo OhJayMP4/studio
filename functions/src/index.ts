c0d681f959e26fe676466124c5fa88aa424d9aab
Ockert Fernandes <ockertfernandes@gmail.com>
2025-10-29 11:04:43 +0000

    Now i get this error
    Invitation Error
    Failed to join workspace: interna

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,8 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as cors from "cors";
+
+const corsHandler = cors({ origin: true });
 
 admin.initializeApp();
 const db = admin.firestore();
